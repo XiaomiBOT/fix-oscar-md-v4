aaafe088350f6c67136dd68839dfae6814fc9871
Xiaomi Team <112295130+XiaomiBOT@users.noreply.github.com>
2023-01-22 13:01:00 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -40,24 +40,24 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᯤ 𝙅𝙖𝙧𝙤𝙩 𝙊𝙛𝙘
+FN: ᯤ RyanOfc
 item.ORG: ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ
-item1.TEL;waid=6285850539404:6285850539404@s.whatsapp.net
+item1.TEL;waid=6285710061796:6285710061796@s.whatsapp.net
 item1.X-ABLabel: ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ
-item2.EMAIL;type=INTERNET: Jarott@gmail.com
+item2.EMAIL;type=INTERNET: hanzteam46@gmail.com
 item2.X-ABLabel:📫 Gmail
 item3.ADR:;;📍 Jawa Timur - Indonesian;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:📮 Location Owner
-item4.URL:https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
+item4.URL:https://youtube.com/channel/ranggabotzofc
 item4.X-ABLabel:Youtube
-item5.URL:https://github.com/Alpiii22
+item5.URL:https://github.com/XiaomiBOT
 item5.X-ABLabel:Github
-item6.URL:https://instagram.com/jarotr_
+item6.URL:https://instagram.com/hanz_ofc_03
 item6.X-ABLabel:Instagram Jarot
-item7.URL:𝚈𝚘𝚞𝚝𝚞𝚋𝚎: 𝙹𝚊𝚛𝚘𝚝 𝙾𝚏𝚏𝚌
-item7.X-ABLabel: 𝚂𝚞𝚜𝚌𝚛𝚒𝚋𝚎 𝙲𝚑𝚊𝚗𝚎𝚕 𝚓𝚊𝚛𝚘𝚝
-item1.TEL;waid=6285943036511:6285850539404@s.whatsapp.net
+item7.URL:𝚈𝚘𝚞𝚝𝚞𝚋𝚎: Rangga Botz Ofc
+item7.X-ABLabel: 𝚂𝚞𝚜𝚌𝚛𝚒𝚋𝚎 𝙲𝚑𝚊𝚗𝚎𝚕 Rangga
+item1.TEL;waid=6285710061796:6285710061796@s.whatsapp.net
 item1.X-ABLabel: Creator Bot
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
